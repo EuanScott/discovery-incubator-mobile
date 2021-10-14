9098ab05641c506362a1e376d9b6fa25703fed24
Euan <euan.workie@gmail.com>
2021-10-14 17:51:48 +0200

    - Updated Retrofit to use RxJava  - Removed LiveData from the app and used RxJava only

--- app/src/main/java/com/example/discoveryincubator/MainActivity.kt
+++ app/src/main/java/com/example/discoveryincubator/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.rxjava3.schedulers.Schedulers
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +20,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        // Listens for when the app retrieves a list of Comic Issues to be able to update the View
-        viewModel.issues.observe(this, {
-            // For test purposes, just log the title of the comics
-            for (i in it) {
-                Log.i(TAG, i.title)
-            }
-        })
+        viewModel.issuesPlsWork
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                {
+                    if (it.isNotEmpty()) {
+                        for (i in it) {
+                            Log.i(TAG, i.title)
+                        }
+                    } else {
+                        displayToast("An unexpected error occurred. Please try again.")
+                    }
+                },
+                { error ->
+                    Log.i("pls", "onError: $error")
+                    displayToast("Unable to fetch Issue data. Please try again")
+                }
+            )
+    }
+
+    // https://stackoverflow.com/a/12897386
+    private fun displayToast(toastMessage: String) {
+        runOnUiThread {
+            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+        }
     }
 }
